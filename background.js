54bff1152df5f4727545610e10c844cdf6b07110
Raj Patel <94529888+RAJ4823@users.noreply.github.com>
2022-09-18 16:57:13 +0530

    Regex updated for phones

--- background.js
+++ background.js
@@ -14,8 +14,8 @@ window.onload = async () => {
     function getData() {
         const body = document.querySelector('body');
         const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/gi;
-        const phoneRegex1 = /^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$/gi;
-        const phoneRegex2 = /^(?:(?:\+|0{0,2})91(\s*[\ -]\s*)?|[0]?)?[789]\d{9}|(\d[ -]?){10}\d$/gi;
+        const phoneRegex1 = /\+\d{1,4}\s?\(?\d{1,2}\)?(?:\s?\-?\d{1,3}){3,5}/gi; //For international numbers
+        const phoneRegex2 = /^(?:(?:\+|0{0,2})91(\s*[\ -]\s*)?|[0]?)?[789]\d{9}|(\d[ -]?){10}\d$/gi; //For indian numbers (telephone/landline)
         let textNode, emails = [], phones = [];
         let ele = document.createTreeWalker(body, NodeFilter.SHOW_TEXT);
         while (textNode = ele.nextNode()) {
